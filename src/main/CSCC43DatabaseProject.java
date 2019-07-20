c0993751ed4a82633c9b0d0e00a270f1917d7d81
Jeremy Lai <jeremylai.14@gmail.com>
2019-07-20 16:56:41 -0400

    Added new DPAPI methods to send a query and get query data based on pre-written query, and added start of Eclipse console interface

--- src/main/CSCC43DatabaseProject.java
+++ src/main/CSCC43DatabaseProject.java
@@ -3,6 +3,7 @@ package main;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,18 +12,36 @@ public class CSCC43DatabaseProject {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		CSCC43DatabaseProject pro = new CSCC43DatabaseProject();
-		pro.createConnection();
+		Connection connection = pro.createConnection();
+		System.out.println("Welcome to the AirBnB database service! You can exit any time by typing 'exit'");
+		Scanner keyboard = new Scanner(System.in);
+		String input = "";
+		while (!input.equalsIgnoreCase("exit")) {
+			input = keyboard.next();
+			if (input.equals("login")) {
+				System.out.println("Welcome!");
+			} else {
+				System.out.print("Please type in 'login' to select a user: ");
+			}
+		}
+		System.out.println("Thank you for using our service!");
+		keyboard.close();
+		if (connection != null) {
+			connection.close();
+		}
 	}
 
-	void createConnection() throws SQLException{
+	Connection createConnection() throws SQLException{
+		Connection con = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con  = DriverManager.getConnection("jdbc:mysql://localhost/air_bnb", "root", "root");
+			con  = DriverManager.getConnection("jdbc:mysql://localhost/air_bnb", "root", "root");
 			System.out.println("Database Connection Success");
 		} catch (ClassNotFoundException ex) {
 			Logger.getLogger(CSCC43DatabaseProject.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (SQLException ex) {
 			Logger.getLogger(CSCC43DatabaseProject.class.getName()).log(Level.SEVERE, null, ex);
 		}
+		return con;
 	}
 }
