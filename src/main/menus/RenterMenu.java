ffa9772d267b4622020cae66ba6851e9421474ae
Jeremy Lai <jeremylai.14@gmail.com>
2019-07-28 00:16:44 -0400

    RenterMenu ready for testing

--- src/main/menus/RenterMenu.java
+++ src/main/menus/RenterMenu.java
@@ -60,7 +60,8 @@ public class RenterMenu extends Menu {
 											Date start = Date.valueOf(reservations.get(resIndex - 1).get("startDate"));
 											Date end = Date.valueOf(reservations.get(resIndex - 1).get("endDate"));
 											String hostSIN = reservations.get(resIndex - 1).get("hostSIN");
-											double price = Double.parseDouble(reservations.get(resIndex - 1).get("price"));
+											double price = Double
+													.parseDouble(reservations.get(resIndex - 1).get("price"));
 											try {
 												operations.renterRemoveListing(this.connection, listing, start, end,
 														this.credentials.get(0), hostSIN, price);
@@ -83,15 +84,131 @@ public class RenterMenu extends Menu {
 						exit = false;
 						break;
 					case 2: // Search for listings
-						System.out.print("Do you want a simple search? (yes/no), or type anything else to exit: ");
-						input = this.keyboard.next();
-						if (input.equalsIgnoreCase("yes")) {
-							// Do the simple search and display
-						} else if (input.equalsIgnoreCase("no")) {
-							// Do the simple search and display
-						} else {
-							break;
+						while (!exit) {
+							boolean postal = false, distance = false, address = false, temporal = false, price = false,
+									amenities = false;
+							String postalValue = null, searchAddress = null, searchAmenities = null;
+							double km = 0, lat = 0, lng = 0, priceL = 0, priceH = 0;
+							Date start = null, end = null;
+							int rankPrice = 1;
+							System.out.print("Do you want a simple search? (yes/no), or type 'exit' to exit: ");
+							input = this.keyboard.next();
+							ArrayList<HashMap<String, String>> results = null;
+							/*
+							 * public static ArrayList<HashMap<String, String>> startFilter(Connection
+							 * connection, boolean postal, String postalValue, boolean distance, double km,
+							 * double lat, double lng, boolean address, String searchAddress, boolean
+							 * temporal, Date start, Date end, boolean price, double priceL, double priceH,
+							 * boolean amenities, String searchAmenities, int rankPrice)
+							 */
+							if (input.equalsIgnoreCase("no") || input.equalsIgnoreCase("yes")) {
+								if (input.equalsIgnoreCase("no")) {
+									System.out.print("Do you want to search for a specific postal code (yes/no): ");
+									input = this.keyboard.nextLine();
+									postal = input.equalsIgnoreCase("yes");
+									if (postal) {
+										System.out.print("Enter the 6 digit postal code you desire: ");
+										postalValue = this.keyboard.nextLine();
+									}
+									System.out.print("Do you want to limit the search radius (yes/no): ");
+									input = this.keyboard.nextLine();
+									distance = input.equalsIgnoreCase("yes");
+									if (distance) {
+										System.out.print("Enter the maximum distance in km you desire: ");
+										km = this.keyboard.nextDouble();
+										System.out.print("Enter the latitude you desire: ");
+										lat = this.keyboard.nextDouble();
+										System.out.print("Enter the longitude you desire: ");
+										lng = this.keyboard.nextDouble();
+									}
+									System.out.print("Do you want to search for a specific address (yes/no): ");
+									input = this.keyboard.nextLine();
+									address = input.equalsIgnoreCase("yes");
+									if (address) {
+										System.out.print("Enter the address you desire: ");
+										searchAddress = this.keyboard.nextLine();
+									}
+									System.out
+											.print("Do you want to specify the date range for availability (yes/no): ");
+									input = this.keyboard.nextLine();
+									temporal = input.equalsIgnoreCase("yes");
+									if (temporal) {
+										System.out.print("Enter your intended start date (YYYY-MM-DD): ");
+										start = Date.valueOf(this.keyboard.nextLine());
+										System.out.print("Enter your intended end date (YYYY-MM-DD): ");
+										end = Date.valueOf(this.keyboard.nextLine());
+									}
+									System.out.print("Do you want to limit the price range (yes/no): ");
+									input = this.keyboard.nextLine();
+									price = input.equalsIgnoreCase("yes");
+									if (price) {
+										System.out.print("Enter the lower price limit: ");
+										priceL = this.keyboard.nextDouble();
+										System.out.print("Enter the upper price limit: ");
+										priceH = this.keyboard.nextDouble();
+									}
+									System.out.print("Do you want to have a specific amenity (limit 1) (yes/no): ");
+									input = this.keyboard.nextLine();
+									amenities = input.equalsIgnoreCase("yes");
+									if (amenities) {
+										System.out.print("Enter the amenity you desire: ");
+										searchAmenities = this.keyboard.nextLine();
+									}
+									System.out.print(
+											"Do you want to sort your searches in 'ascending', 'descending' order, type anything else for no preference: ");
+									input = this.keyboard.nextLine();
+									if (input.equalsIgnoreCase("ascending")) {
+										rankPrice = 1;
+									} else if (input.equalsIgnoreCase("descending")) {
+										rankPrice = 2;
+									}
+								}
+								try {
+
+									int selection = -1;
+									while (selection != 0) {
+										results = queries.startFilter(this.connection, postal, postalValue, distance,
+												km, lat, lng, address, searchAddress, temporal, start, end, price,
+												priceL, priceH, amenities, searchAmenities, rankPrice);
+										this.printQueries(results);
+										System.out.print(
+												"Enter the number of a reservation to focus on, or '0' to exit: ");
+										selection = this.keyboard.nextInt();
+										if (selection > results.size() || selection < 0) {
+											System.err.print("Not a valid index!");
+										} else if (selection != 0) {
+											System.out.print("Enter 'book' to book this listing, 'comment' to comment on the host, 'rate' to rate the listing, anything else to exit: ");
+											input = this.keyboard.nextLine();
+											switch(input) {
+											case "book":
+												operations.bookListing(this.connection, this.credentials.get(0), results.get(selection - 1).get("listingID"), start, end);
+												System.out.println("Booking has been made.");
+											case "comment":
+												System.out.println("Type the comment you want to make below, then hit enter.");
+												String message = this.keyboard.next();
+												operations.addComment(this.connection, this.credentials.get(0), results.get(selection - 1).get("hostSIN"), message);
+											case "rate":
+												System.out.println("Enter a rating from 0-5: ");
+												double rating = this.keyboard.nextDouble();
+												System.out.println("Type the comment you want to make below, then hit enter.");
+												String comment = this.keyboard.next();
+												operations.addRating(this.connection, this.credentials.get(0), comment, Double.toString(rating), results.get(selection - 1).get("listingID"));
+											default:
+												break;
+											}
+										}
+									}
+								} catch (SQLException e) {
+									System.err.println(
+											"An unexpected error has occurred while processing your request. Please try again");
+								}
+							} else if (input.equalsIgnoreCase("exit")) {
+								exit = true;
+							}
+
 						}
+						exit = false;
+						break;
 					case 3: // View booking history
 						try {
 							while (!exit) {
@@ -132,7 +249,8 @@ public class RenterMenu extends Menu {
 																									// get the right
 																									// attribute
 											} catch (SQLException e) {
-												System.err.println("No interaction exists between you and the host: Illegal Operation");
+												System.err.println(
+														"No interaction exists between you and the host: Illegal Operation");
 											}
 										} else if (input.equalsIgnoreCase("0")) {
 											exit = true;
@@ -190,6 +308,30 @@ public class RenterMenu extends Menu {
 			}
 		}
 
+		System.out
+				.println("-------------------------------------------------------------------------------------------");
+	}
+
+	private void printQueries(ArrayList<HashMap<String, String>> results) {
+		System.out.printf(
+				"Results: %-10s%-9s%-10s%-5s%-5s%-25s%-25s%-35s%-10s%-10s%-10s%-10s%-10s%-20s%-20s%-20s%-20s%-20s%n",
+				"listingID", "hostSIN", "type", "longitude", "latitude", "city", "country", "address", "postalCode",
+				"dining", "safetyFeatures", "facilities", "guestAccess", "logistics", "notIncluded", "bedAndBath",
+				"outdoor", "basic");
+		System.out
+				.println("-------------------------------------------------------------------------------------------");
+		for (int i = 0; i < results.size(); i++) {
+			System.out.printf(
+					"%-10d%-10s%-9s%-10s%-5s%-5s%-25s%-25s%-35s%-10s%-10s%-10s%-10s%-10s%-20s%-20s%-20s%-20s%-20s%n",
+					i + 1, (results.get(i)).get("listingID"), (results.get(i)).get("hostSIN"),
+					(results.get(i)).get("type"), (results.get(i)).get("longitude"), (results.get(i)).get("latitude"),
+					(results.get(i)).get("city"), (results.get(i)).get("country"), (results.get(i)).get("address"),
+					(results.get(i)).get("postalCode"), (results.get(i)).get("dining"),
+					(results.get(i)).get("safetyFeatures"), (results.get(i)).get("facilities"),
+					(results.get(i)).get("guessAccess"), (results.get(i)).get("logistics"),
+					(results.get(i)).get("notIncluded"), (results.get(i)).get("bedAndBath"),
+					(results.get(i)).get("outdoor"), (results.get(i)).get("basic"));
+		}
 		System.out
 				.println("-------------------------------------------------------------------------------------------");
 	}
